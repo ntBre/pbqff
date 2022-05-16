e5bace90568bfaedc84d49e580a05fc9997c41b7
Brent Westbrook <brentrwestbrook@gmail.com>
2022-05-15 22:21:49 -0500

    start outlining code for next steps

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,10 @@ pub mod config;
 
 use psqs::program::Template;
 
+/// step size to take in each symmetry internal coordinate to determine its
+/// irrep
+pub const TAYLOR_DISP_SIZE: f64 = 0.005;
+
 // TODO take this from a template file
 pub const MOPAC_TMPL: Template = Template::from(
     "scfcrt=1.D-21 aux(precision=14) PM6 external=testfiles/params.dat",
@@ -17,8 +21,8 @@ mod tests {
         queue::{local::LocalQueue, Queue},
     };
 
-    use crate::{config::Config, MOPAC_TMPL};
-    use psqs::atom::Geom;
+    use crate::{config::Config, MOPAC_TMPL, TAYLOR_DISP_SIZE};
+    use psqs::geom::Geom;
 
     #[test]
     fn test_full() {
@@ -46,16 +50,27 @@ mod tests {
         std::fs::remove_dir_all("opt").unwrap();
         println!("{}", geom);
 
+        let pg =
+            symm::Molecule::new(geom.xyz().unwrap().to_vec()).point_group();
+        dbg!(pg);
+
         let intder = Intder::load_file("testfiles/intder.in");
-        // DONE convert psqs::Geom -> intder::Geom
+        let nsic = intder.symmetry_internals.len();
+        let mut disps = Vec::new();
+        for i in 0..nsic {
+            let mut disp = vec![0.0; nsic];
+            disp[i] = TAYLOR_DISP_SIZE;
+            disps.push(disp);
+        }
         let symm_intder = Intder {
             geom: intder::geom::Geom::from(geom),
+            disps,
             ..intder.clone()
         };
         // TODO do displacements for symmetries
         // TODO convert displacements -> symm::Molecules
-	// TODO generate taylor.py input from that
-	// TODO run taylor.py to get fcs and disps
-        dbg!(intder);
+        // TODO generate taylor.py input from that
+        // TODO run taylor.py to get fcs and disps
+        dbg!(symm_intder);
     }
 }
