455310e5b7e7d37efa8d71f27d551d2f23f182d0
Brent Westbrook <brentrwestbrook@gmail.com>
2024-11-30 16:15:01 -0500

    add assert_cmd and insta and add basic assert_cmd test for cart

---
+++ tests/cart.rs
@@ -0,0 +1,23 @@
+use std::{fs::read_to_string, path::Path};
+
+use assert_cmd::Command;
+use tempfile::tempdir;
+
+#[test]
+fn cart() -> std::io::Result<()> {
+    let config_file = Path::new("testfiles/cart.toml");
+    let dir = tempdir()?;
+    std::fs::copy(config_file, dir.path().join("cart.toml"))?;
+    let mut cmd = Command::cargo_bin("pbqff").unwrap();
+    let assert = cmd.arg("cart.toml").current_dir(&dir).assert();
+    let output = assert.get_output();
+
+    assert!(
+        output.status.success(),
+        "stderr: {}\nlog: {}",
+        String::from_utf8_lossy(&output.stderr),
+        read_to_string(dir.path().join("pbqff.log"))?,
+    );
+
+    Ok(())
+}
