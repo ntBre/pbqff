ec63c82b944ff22c3ddb5c30a2f6d65b3768ca72
Brent Westbrook <brentrwestbrook@gmail.com>
2024-11-30 17:09:22 -0500

    redact numerical results from cart snapshot

--- tests/cart.rs
+++ tests/cart.rs
@@ -20,9 +20,18 @@ fn cart() -> std::io::Result<()> {
         read_to_string(dir.path().join("pbqff.log"))?,
     );
 
+    // filter out essentially all of the numerical results just to check that
+    // all of the sections are there
     with_settings!(
         {filters => vec![
-            (r"PID: \d+", "PID: [PID]"),
+            (r"(?m)^PID: \d+$", "PID: [PID]"),
+            (r"(?s)(normalized geometry:).*(point group)", "$1[Normalized Geometry]\n$2"),
+            (r"(?ms)^STATE NO.*?^$", "[Vibrational States]"),
+            (r"(?s)(Transformed Geometry \(Å\):).*(Equilibrium)", "$1\n[Transformed Geometry]\n$2"),
+            (r"(?ms)(Equilibrium Rotational Constants \(cm-1\):).*?^$", "$1\n[Rotational Constants]\n"),
+            (r"(?s)(Geometry:).*(Vibrational)", "$1\n[Geometry]\n$2"),
+            (r"(?s)(Vibrational Frequencies \(cm-1\):).*?(ZPT)", "$1\n[Freqs]\n$2"),
+            (r"(?ms)(^Rotational Constants \(cm-1\):).*?(Coriolis Resonances)", "$1\n[Rotational Constants]\n$2"),
         ]}, {
             assert_snapshot!(read_to_string(dir.path().join("pbqff.out")).unwrap());
         }
