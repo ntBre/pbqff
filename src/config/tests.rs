3860882c9454890e511a2d59547a56523956be0a
Brent Westbrook <brentrwestbrook@gmail.com>
2024-11-30 20:58:11 -0500

    use local queue in normal.toml, set insta suffixes
    to get reproducible ordering of snapshots

--- src/config/tests.rs
+++ src/config/tests.rs
@@ -1,11 +1,13 @@
-use insta::assert_debug_snapshot;
+use insta::{assert_debug_snapshot, with_settings};
 use test_case::test_case;
 
 use super::*;
 
-#[test_case("testfiles/test.toml")]
-#[test_case("testfiles/path.toml")]
-#[test_case("testfiles/normal.toml")]
+#[test_case("testfiles/test.toml" ; "cart")]
+#[test_case("testfiles/path.toml" ; "path templates")]
+#[test_case("testfiles/normal.toml" ; "norm")]
 fn load_config(path: &str) {
-    assert_debug_snapshot!(Config::load(path));
+    with_settings!({ snapshot_suffix => path }, {
+        assert_debug_snapshot!(Config::load(path));
+    });
 }
