3f1fb1fa5aa7b4e2d8ce2f2754acd45f0716c596
Brent Westbrook <brentrwestbrook@gmail.com>
2024-11-30 21:02:36 -0500

    add cart config case, update test.toml description to reflect sic

--- src/config/tests.rs
+++ src/config/tests.rs
@@ -3,9 +3,10 @@ use test_case::test_case;
 
 use super::*;
 
-#[test_case("testfiles/test.toml" ; "cart")]
+#[test_case("testfiles/test.toml" ; "basic sic")]
+#[test_case("testfiles/cart.toml" ; "basic cart")]
+#[test_case("testfiles/normal.toml" ; "basic norm")]
 #[test_case("testfiles/path.toml" ; "path templates")]
-#[test_case("testfiles/normal.toml" ; "norm")]
 fn load_config(path: &str) {
     with_settings!({ snapshot_suffix => path }, {
         assert_debug_snapshot!(Config::load(path));
