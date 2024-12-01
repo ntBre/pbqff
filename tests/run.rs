8b46fd797991c82ebfc2f4f5e50ac9d5e4aeef43
Brent Westbrook <brentrwestbrook@gmail.com>
2024-11-30 21:44:48 -0500

    add norm.toml as copy of normal without resume_hff, parameterize
    run to cover the other coordinate types

--- tests/run.rs
+++ tests/run.rs
@@ -3,14 +3,22 @@ use std::{fs::read_to_string, path::Path};
 use assert_cmd::Command;
 use insta::{assert_snapshot, with_settings};
 use tempfile::tempdir;
+use test_case::test_case;
 
-#[test]
-fn cart() -> std::io::Result<()> {
-    let config_file = Path::new("testfiles/cart.toml");
+#[test_case("testfiles/test.toml", &["testfiles/intder.in"]; "sic")]
+#[test_case("testfiles/cart.toml", &[]; "cart")]
+#[test_case("testfiles/norm.toml", &[]; "norm")]
+fn run(path: &str, other_files: &[&str]) -> std::io::Result<()> {
+    let config_file = Path::new(path);
     let dir = tempdir()?;
-    std::fs::copy(config_file, dir.path().join("cart.toml"))?;
+    std::fs::copy(config_file, dir.path().join("pbqff.toml"))?;
+    for file in other_files {
+        let p = Path::new(file);
+        let filename = p.file_name().unwrap();
+        std::fs::copy(file, dir.path().join(filename))?;
+    }
     let mut cmd = Command::cargo_bin("pbqff").unwrap();
-    let assert = cmd.arg("cart.toml").current_dir(&dir).assert();
+    let assert = cmd.arg("pbqff.toml").current_dir(&dir).assert();
     let output = assert.get_output();
 
     assert!(
@@ -22,20 +30,23 @@ fn cart() -> std::io::Result<()> {
 
     // filter out essentially all of the numerical results just to check that
     // all of the sections are there
-    with_settings!(
-        {filters => vec![
+    with_settings!({
+        filters => vec![
             (r"(?m)^PID: \d+$", "PID: [PID]"),
             (r"(?s)(normalized geometry:).*(point group)", "$1[Normalized Geometry]\n$2"),
+            (r"(?ms)^Normal Coordinates:.*?^$", "[Normal Coordinates]"),
             (r"(?ms)^STATE NO.*?^$", "[Vibrational States]"),
             (r"(?s)(Transformed Geometry \(Å\):).*(Equilibrium)", "$1\n[Transformed Geometry]\n$2"),
             (r"(?ms)(Equilibrium Rotational Constants \(cm-1\):).*?^$", "$1\n[Rotational Constants]\n"),
             (r"(?s)(Geometry:).*(Vibrational)", "$1\n[Geometry]\n$2"),
             (r"(?s)(Vibrational Frequencies \(cm-1\):).*?(ZPT)", "$1\n[Freqs]\n$2"),
             (r"(?ms)(^Rotational Constants \(cm-1\):).*?(Coriolis Resonances)", "$1\n[Rotational Constants]\n$2"),
-        ]}, {
-            assert_snapshot!(read_to_string(dir.path().join("pbqff.out")).unwrap());
-        }
-    );
+        ],
+        snapshot_suffix => path
+
+    }, {
+        assert_snapshot!(read_to_string(dir.path().join("pbqff.out")).unwrap());
+    });
 
     Ok(())
 }
