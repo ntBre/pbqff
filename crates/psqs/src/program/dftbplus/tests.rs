4d4deae23ff8c59d9086929b5666e79ce6b795d3
Brent Westbrook <brentrwestbrook@gmail.com>
2025-03-08 12:50:46 -0500

    move dftb+ tests to snapshots

--- crates/psqs/src/program/dftbplus/tests.rs
+++ crates/psqs/src/program/dftbplus/tests.rs
@@ -1,8 +1,8 @@
 use std::str::FromStr;
 
-use super::*;
+use insta::{assert_debug_snapshot, assert_snapshot};
 
-use crate::check;
+use super::*;
 
 #[test]
 fn write_input() {
@@ -55,10 +55,95 @@ Geometry Step: 9
     };
 
     d.write_input(Procedure::Opt);
-    check!("testfiles/dftb+/single_opt.want", "/tmp/dftb_in.hsd");
+
+    assert_snapshot!(read_to_string("/tmp/dftb_in.hsd").unwrap(), @r#"
+    Geometry = xyzFormat {
+    3
+
+    O       0.0000000000  -0.7160331500   0.0000000000
+    H       0.0000000000  -0.1420029800   0.7784480400
+    H      -0.0000000000  -0.1420029800  -0.7784480400
+
+
+    }
+
+    Hamiltonian = DFTB {
+      Scc = Yes
+      SlaterKosterFiles = Type2FileNames {
+        Prefix = "/opt/dftb+/slako/mio/mio-1-1/"
+        Separator = "-"
+        Suffix = ".skf"
+      }
+      MaxAngularMomentum {
+        O = "p"
+        H = "s"
+      }
+      Charge = 0
+    }
+
+    Options {
+    }
+
+    Analysis {
+      CalculateForces = Yes
+    }
+
+    ParserOptions {
+      ParserVersion = 12
+    }
+    Driver = GeometryOptimization {
+      Optimizer = Rational {}
+      MovedAtoms = 1:-1
+      MaxSteps = 100
+      OutputPrefix = "geom.out"
+      Convergence {
+            Energy = 1e-8
+            GradElem = 1e-7
+            GradNorm = 1e-7
+            DispElem = 1e-7
+            DispNorm = 1e-7
+    }
+    }
+    "#);
 
     d.write_input(Procedure::SinglePt);
-    check!("testfiles/dftb+/single_single.want", "/tmp/dftb_in.hsd");
+
+    assert_snapshot!(read_to_string("/tmp/dftb_in.hsd").unwrap(), @r#"
+    Geometry = xyzFormat {
+    3
+
+    O       0.0000000000  -0.7160331500   0.0000000000
+    H       0.0000000000  -0.1420029800   0.7784480400
+    H      -0.0000000000  -0.1420029800  -0.7784480400
+
+
+    }
+
+    Hamiltonian = DFTB {
+      Scc = Yes
+      SlaterKosterFiles = Type2FileNames {
+        Prefix = "/opt/dftb+/slako/mio/mio-1-1/"
+        Separator = "-"
+        Suffix = ".skf"
+      }
+      MaxAngularMomentum {
+        O = "p"
+        H = "s"
+      }
+      Charge = 0
+    }
+
+    Options {
+    }
+
+    Analysis {
+      CalculateForces = Yes
+    }
+
+    ParserOptions {
+      ParserVersion = 12
+    }
+    "#);
 
     // test that we can also handle a provided geometry driver
     let template = Template::from(
@@ -123,34 +208,139 @@ Geometry Step: 9
     };
 
     d.write_input(Procedure::Opt);
-    check!("testfiles/dftb+/opt_opt.want", "/tmp/dftb_in.hsd");
+    assert_snapshot!(read_to_string("/tmp/dftb_in.hsd").unwrap(), @r#"
+    Geometry = xyzFormat {
+    3
+
+    O       0.0000000000  -0.7160331500   0.0000000000
+    H       0.0000000000  -0.1420029800   0.7784480400
+    H      -0.0000000000  -0.1420029800  -0.7784480400
+
+
+    }
+
+    Hamiltonian = DFTB {
+      Scc = Yes
+      SlaterKosterFiles = Type2FileNames {
+        Prefix = "/opt/dftb+/slako/mio/mio-1-1/"
+        Separator = "-"
+        Suffix = ".skf"
+      }
+      MaxAngularMomentum {
+        O = "p"
+        H = "s"
+      }
+      Charge = 0
+    }
+
+    Options {
+    }
+
+    Analysis {
+      CalculateForces = Yes
+    }
+
+    ParserOptions {
+      ParserVersion = 12
+    }
+    Driver = GeometryOptimization {
+      Optimizer = Rational {}
+      MovedAtoms = 1:-1
+      MaxSteps = 100
+      OutputPrefix = "geom.out"
+      Convergence {
+            Energy = 1e-8
+            GradElem = 1e-8
+            GradNorm = 1e-7
+            DispElem = 1e-7
+            DispNorm = 1e-7
+    }
+    }
+    "#);
 
     d.write_input(Procedure::SinglePt);
-    check!("testfiles/dftb+/single_single.want", "/tmp/dftb_in.hsd");
+    assert_snapshot!(read_to_string("/tmp/dftb_in.hsd").unwrap(), @r#"
+    Geometry = xyzFormat {
+    3
+
+    O       0.0000000000  -0.7160331500   0.0000000000
+    H       0.0000000000  -0.1420029800   0.7784480400
+    H      -0.0000000000  -0.1420029800  -0.7784480400
+
+
+    }
+
+    Hamiltonian = DFTB {
+      Scc = Yes
+      SlaterKosterFiles = Type2FileNames {
+        Prefix = "/opt/dftb+/slako/mio/mio-1-1/"
+        Separator = "-"
+        Suffix = ".skf"
+      }
+      MaxAngularMomentum {
+        O = "p"
+        H = "s"
+      }
+      Charge = 0
+    }
+
+    Options {
+    }
+
+    Analysis {
+      CalculateForces = Yes
+    }
+
+    ParserOptions {
+      ParserVersion = 12
+    }
+    "#);
 }
 
 #[test]
 fn read_opt_output() {
     let got = DFTBPlus::read_output("testfiles/dftb+/opt").unwrap();
-    let want = ProgramResult {
+    assert_debug_snapshot!(got, @r"
+    ProgramResult {
         energy: -4.0779379326,
-        cart_geom: Some(vec![
-            Atom::new_from_label("O", 0.00000000, -0.71603315, 0.00000000),
-            Atom::new_from_label("H", 0.00000000, -0.14200298, 0.77844804),
-            Atom::new_from_label("H", -0.00000000, -0.14200298, -0.77844804),
-        ]),
+        cart_geom: Some(
+            [
+                Atom {
+                    atomic_number: 8,
+                    x: 0.0,
+                    y: -0.71603315,
+                    z: 0.0,
+                    weight: None,
+                },
+                Atom {
+                    atomic_number: 1,
+                    x: 0.0,
+                    y: -0.14200298,
+                    z: 0.77844804,
+                    weight: None,
+                },
+                Atom {
+                    atomic_number: 1,
+                    x: -0.0,
+                    y: -0.14200298,
+                    z: -0.77844804,
+                    weight: None,
+                },
+            ],
+        ),
         time: 0.05,
-    };
-    assert_eq!(got, want);
+    }
+    ");
 }
 
 #[test]
 fn read_single_output() {
     let got = DFTBPlus::read_output("testfiles/dftb+/single").unwrap();
-    let want = ProgramResult {
+    assert_debug_snapshot!(got, @r"
+    ProgramResult {
         energy: -3.9798793068,
         cart_geom: None,
         time: 0.03,
-    };
-    assert_eq!(got, want);
+    }
+    ");
 }
